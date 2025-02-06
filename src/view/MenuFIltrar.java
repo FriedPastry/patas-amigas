b82ea0ff5b48292dc81c61b2cb1b7884509051a4
Yasushis <viniciusmiura1@gmail.com>
2025-02-06 19:33:28 -0300

    bugfix filtrar

--- src/view/MenuFIltrar.java
+++ src/view/MenuFIltrar.java
@@ -33,6 +33,10 @@ public class MenuFIltrar extends Menu{
         System.out.println("|Nome" + " ".repeat(maxNome-4) + "|Data Nascimento" + " ".repeat(maxData - 15) + "|Email" + " ".repeat(maxEmail - 5) + "|Endereço" + " ".repeat(maxEndereco - 8) + "|Gênero" + " ".repeat(maxGenero - 6) + "|CPF" + " ".repeat(maxCPF - 3) + "|Telefone" + " ".repeat(maxTelefone - 8) + "|Senha" + " ".repeat(maxSenha - 5) + "|" + "Adotante|Tutor|Funcionário|");
         System.out.println("|"+ pessoa_pesquisada.getNome() + " ".repeat(maxNome-pessoa_pesquisada.getNome().length()) + "|" + pessoa_pesquisada.getDataNascimento() + " ".repeat(maxData - pessoa_pesquisada.getDataNascimento().length()) + "|" + pessoa_pesquisada.getEmail() + " ".repeat(maxEmail - pessoa_pesquisada.getEmail().length()) + "|" + pessoa_pesquisada.getEndereco() + " ".repeat(maxEndereco - pessoa_pesquisada.getEndereco().length()) + "|" + pessoa_pesquisada.getGenero() + " ".repeat(maxGenero - pessoa_pesquisada.getGenero().length()) + "|" + pessoa_pesquisada.getCPF() + " ".repeat(maxCPF - pessoa_pesquisada.getCPF().length()) + "|" + pessoa_pesquisada.getTelefone() + " ".repeat(maxTelefone - pessoa_pesquisada.getTelefone().length()) + "|" + pessoa_pesquisada.getSenha() + " ".repeat(maxSenha - pessoa_pesquisada.getSenha().length()) + "|" + Boolean.toString(pessoa_pesquisada.isAdotante()).charAt(0) + "       |" + Boolean.toString(pessoa_pesquisada.isTutor()).charAt(0) + "    |" + Boolean.toString(pessoa_pesquisada.isFuncionario()).charAt(0) + "          |");
 
+
+        System.out.println();
+        exibirMenuPrincipal();
+
     }
     
 }
